dc5ecc7bdb4b84ffb45f50c90e5c48e153ee17b3
atash <atash@LAPTOP-R2LDVLRR>
2025-01-28 20:14:42 -0600

    Enhance image gallery in Page1 component with toggleable info display; update styles for improved layout and interactivity

--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -6,23 +6,44 @@ import { MessageTestComponent } from '../components/message-test/message-test.co
   selector: 'app-page1',
   templateUrl: './page1.component.html',
   styleUrls: ['./page1.component.scss'],
+  standalone: true,
   imports: [CommonModule, MessageTestComponent]
 })
 export class Page1Component {
-  // images: string[] = [
-  //   'assets/images/image1.jpeg',
-  //   'assets/images/image2.jpeg',
-  //   'assets/images/image3.jpeg',
-  //   'assets/images/image4.jpg',
-  //   'assets/images/image5.jpg',
-  //   'assets/images/image6.jpg',
-  // ];
+
   images = [
-    { src: 'assets/images/image1.jpeg', alt: 'Image 1', message: 'Message for Image 1' },
-    { src: 'assets/images/image2.jpeg', alt: 'Image 2', message: 'Message for Image 2' },
-    { src: 'assets/images/image3.jpeg', alt: 'Image 3', message: 'Message for Image 3' },
-    { src: 'assets/images/image4.jpg', alt: 'Image 4', message: 'Message for Image 4' },
-    { src: 'assets/images/image5.jpg', alt: 'Image 5', message: 'Message for Image 5' },
-    { src: 'assets/images/image6.jpg', alt: 'Image 6', message: 'Message for Image 6' },
+    { src: 'assets/images/image1.jpeg', alt: 'Image 1', message: 'Message for Image 1' , info: 'This is image 1 description.', showInfo: false },
+    { src: 'assets/images/image2.jpeg', alt: 'Image 2', message: 'Message for Image 2', info: 'This is image 1 description.', showInfo: false  },
+    { src: 'assets/images/image3.jpeg', alt: 'Image 3', message: 'Message for Image 3', info: 'This is image 1 description.', showInfo: false  },
+    { src: 'assets/images/image4.jpg', alt: 'Image 4', message: 'Message for Image 4', info: 'This is image 1 description.', showInfo: false  },
+    { src: 'assets/images/image5.jpg', alt: 'Image 5', message: 'Message for Image 5', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image6.jpg', alt: 'Image 6', message: 'Message for Image 6', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image7.jpeg', alt: 'Image 1', message: 'Message for Image 1', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image8.jpeg', alt: 'Image 2', message: 'Message for Image 2', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image9.jpeg', alt: 'Image 3', message: 'Message for Image 3', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image10.jpeg', alt: 'Image 4', message: 'Message for Image 4', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image11.jpeg', alt: 'Image 5', message: 'Message for Image 5', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image12.jpeg', alt: 'Image 6', message: 'Message for Image 6', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image13.jpeg', alt: 'Image 4', message: 'Message for Image 4', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image14.jpeg', alt: 'Image 5', message: 'Message for Image 5', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image15.jpeg', alt: 'Image 6', message: 'Message for Image 6', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image4.jpg', alt: 'Image 4', message: 'Message for Image 4', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image5.jpg', alt: 'Image 5', message: 'Message for Image 5', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image6.jpg', alt: 'Image 6', message: 'Message for Image 6', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image7.jpeg', alt: 'Image 1', message: 'Message for Image 1', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image8.jpeg', alt: 'Image 2', message: 'Message for Image 2', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image9.jpeg', alt: 'Image 3', message: 'Message for Image 3', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image10.jpeg', alt: 'Image 4', message: 'Message for Image 4', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image11.jpeg', alt: 'Image 5', message: 'Message for Image 5', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image10.jpeg', alt: 'Image 4', message: 'Message for Image 4', info: 'This is image 1 description.', showInfo: false   },
+    { src: 'assets/images/image11.jpeg', alt: 'Image 5', message: 'Message for Image 5', info: 'This is image 1 description.', showInfo: false   },
   ];
+  
+  toggleInfo(image: any) {
+    image.showInfo = !image.showInfo;
+    console.log('Image clicked:', image.alt, 'showInfo:', image.showInfo);
+    
+  }
+
+  
 }
